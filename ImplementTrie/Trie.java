3fdbbc2ebf41956f00e73fd1e3e3c9285add34eb
yf23 <yf23@outlook.com>
2015-11-15 01:08:34 -0800

    recursive solution

--- ImplementTrie/Trie.java
+++ ImplementTrie/Trie.java
@@ -1,29 +1,30 @@
-/*	
-	Implement Trie (Prefix Tree)
-	
-	Implement a trie with insert, search, and startsWith methods.
+/*  
+    Implement Trie (Prefix Tree)
+    
+    Implement a trie with insert, search, and startsWith methods.
 
-	Note:
-	You may assume that all inputs are consist of lowercase letters a-z.
+    Note:
+    You may assume that all inputs are consist of lowercase letters a-z.
 
-	Your Trie object will be instantiated and called as such:
-	trie.insert("somestring");
-	trie.search("key");
+    Your Trie object will be instantiated and called as such:
+    trie.insert("somestring");
+    trie.search("key");
 
-	Yu Fu, Nov 14 2015
+    Yu Fu, Nov 14 2015
  */
 
-// Implementation of TrieNode.
+// Implementation of TrieNode
 class TrieNode {
-    String word;
+    // Initialize your data structure here.
+    boolean isWord;
     TrieNode[] children;
     
     public TrieNode() {
-        children = new TrieNode[27];
+        children = new TrieNode[26];
     }
 }
 
-// Implementation of Trie tree.
+// Implementation of Trie tree
 public class Trie {
     private TrieNode root;
 
@@ -33,64 +34,47 @@ public class Trie {
 
     // Inserts a word into the trie.
     public void insert(String word) {
-        TrieNode current = root;
-        String wordCopy = word;
-        while (word.length() != 0) {
-            if (current.children[word.charAt(0) - 'a'] == null) {
-                current.children[word.charAt(0) - 'a'] = new TrieNode();
+        insertHelper(word, root);
+    }
+    
+    private void insertHelper(String sequence, TrieNode current) {
+        if (sequence.length() > 0) {
+            int index = (int) (sequence.charAt(0) - 'a');
+            if (current.children[index] == null) {
+                current.children[index] = new TrieNode();
             }
-            current = current.children[word.charAt(0) - 'a'];
-            word = word.substring(1);
+            insertHelper(sequence.substring(1), current.children[index]);
+        } else {
+            current.isWord = true;       
         }
-        while (current.word != null) {
-            if (current.word.equals(wordCopy)) {
-                return;
-            } else {
-                if (current.children[26] == null) {
-                    current.children[26] = new TrieNode();
-                }
-                current = current.children[26];
-            }
-        }
-        current.word = wordCopy;
     }
 
     // Returns if the word is in the trie.
     public boolean search(String word) {
-        TrieNode current = root;
-        String wordCopy = word;
-        while (word.length() != 0) {
-            current = current.children[word.charAt(0) - 'a'];
-            if (current == null) return false;
-            word = word.substring(1);
-        }
-        while (current != null) {
-            if (!wordCopy.equals(current.word)) {
-                current = current.children[26];
-            } else {
-                return true;
-            }
-        }
-        return false;
+        return searchHelper(word, root);
+    }
+    
+    private boolean searchHelper(String sequence, TrieNode current) {
+        if (current == null) return false;
+        if (sequence.length() == 0) return current.isWord;
+        return searchHelper(sequence.substring(1), current.children[sequence.charAt(0) - 'a']);
     }
 
     // Returns if there is any word in the trie
     // that starts with the given prefix.
     public boolean startsWith(String prefix) {
-        TrieNode current = root;
-        while (prefix.length() != 0) {
-            current = current.children[prefix.charAt(0) - 'a'];
-            if (current == null) return false;
-            prefix = prefix.substring(1);
-        }
-        if (current != null) {
-            if (current.word != null) return true;
+        return startsWithHelper(prefix, root);
+    }
+    
+    private boolean startsWithHelper(String sequence, TrieNode current) {
+        if (current == null) return false;
+        if (sequence.length() == 0) {
+            boolean haveChildren = false;
             for (TrieNode child : current.children) {
-                if (child != null) {
-                    return true;
-                }
+                haveChildren = haveChildren || (child != null);
             }
+            return haveChildren || current.isWord;
         }
-        return false;
+        return startsWithHelper(sequence.substring(1), current.children[sequence.charAt(0) - 'a']);
     }
 }
