efc59b3fa416d16519b3ae599c76becb7860dee3
yf23 <yf23@outlook.com>
2015-11-23 22:15:04 -0800

    change format

--- RangeSumQuery2DImmutable/NumMatrix.java
+++ RangeSumQuery2DImmutable/NumMatrix.java
@@ -1,43 +1,43 @@
 /* 
     Range Sum Query 2D - Immutable
 
-	Given a 2D matrix matrix, find the sum of the elements inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+    Given a 2D matrix matrix, find the sum of the elements inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
 
-	Example:
-	Given matrix = [
-	  [3, 0, 1, 4, 2],
-	  [5, 6, 3, 2, 1],
-	  [1, 2, 0, 1, 5],
-	  [4, 1, 0, 1, 7],
-	  [1, 0, 3, 0, 5]
-	]
+    Example:
+    Given matrix = [
+      [3, 0, 1, 4, 2],
+      [5, 6, 3, 2, 1],
+      [1, 2, 0, 1, 5],
+      [4, 1, 0, 1, 7],
+      [1, 0, 3, 0, 5]
+    ]
 
-	sumRegion(2, 1, 4, 3) -> 8
-	sumRegion(1, 1, 2, 2) -> 11
-	sumRegion(1, 2, 2, 4) -> 12
+    sumRegion(2, 1, 4, 3) -> 8
+    sumRegion(1, 1, 2, 2) -> 11
+    sumRegion(1, 2, 2, 4) -> 12
 
-	Note:
-	You may assume that the matrix does not change.
-	There are many calls to sumRegion function.
-	You may assume that row1 ≤ row2 and col1 ≤ col2.
+    Note:
+    You may assume that the matrix does not change.
+    There are many calls to sumRegion function.
+    You may assume that row1 ≤ row2 and col1 ≤ col2.
 
-	Your NumMatrix object will be instantiated and called as such:
-	NumMatrix numMatrix = new NumMatrix(matrix);
-	numMatrix.sumRegion(0, 1, 2, 3);
-	numMatrix.sumRegion(1, 2, 3, 4);
-	 _____________
+    Your NumMatrix object will be instantiated and called as such:
+    NumMatrix numMatrix = new NumMatrix(matrix);
+    numMatrix.sumRegion(0, 1, 2, 3);
+    numMatrix.sumRegion(1, 2, 3, 4);
+     _____________
     |      |      |
     |  A1  |  A2  |
-   	|______|______|
+    |______|______|
     |      |      |
     |  A3  |  A4  |
-	|______|______|
+    |______|______|
 
-	Build sumMatrix:
-	sum(A1 + A2 + A3 + A4) = sum(A1 + A2) + sum(A1 + A3) - A1 + A4
-	
-	Caculate sumRegion:
-	A4 = sum(A1 + A2 + A3 + A4) - sum(A1 + A2) - sum(A1 + A3) + A1
+    Build sumMatrix:
+    sum(A1 + A2 + A3 + A4) = sum(A1 + A2) + sum(A1 + A3) - A1 + A4
+    
+    Caculate sumRegion:
+    A4 = sum(A1 + A2 + A3 + A4) - sum(A1 + A2) - sum(A1 + A3) + A1
               
     Yu Fu, Nov 23 2015  
 */
